9a3cf10ef25488a30f6f4bf562ea6d05053c2349
Eric Shirley <eric.s.shirley@gmail.com>
2024-10-29 08:36:01 -0400

    svelte-flow rendering

--- example/src/routes/+page.svelte
+++ example/src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+import '@xyflow/svelte/dist/style.css';
+
+import { Background, Controls, SvelteFlow } from '@xyflow/svelte';
+    import type { Node } from '@xyflow/svelte';
+    import type { Edge } from '@xyflow/svelte';
+    import { writable, type Writable } from 'svelte/store';
+
+
+const nodes: Writable<Node[]> = writable([
+  {
+    id: '1', // required and needs to be a string
+    position: { x: 0, y: 0 }, // required
+    data: { label: 'hey' }, // required
+  },
+  {
+    id: '2',
+    position: { x: 100, y: 100 },
+    data: { label: 'world' },
+  },]);
+const edges: Writable<Edge[]> = writable([
+{ id: '1-2', source: '1', target: '2' }
+]);
+
+</script>
+
+<main>
+<SvelteFlow {nodes} {edges} id="tweakometry">
+	<Background />
+	<Controls />
+</SvelteFlow>
+</main>
+
+<style>
+main {
+	width: 100vw;
+	height: 100vh;
+}
+</style>
+
--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,6 +1,15 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig, searchForWorkspaceRoot } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+
+	server: {
+		fs: {
+			allow: [
+				// https://vitejs.dev/config/#server-fs-allow
+				'..',
+			],
+		},
+	},
 });
